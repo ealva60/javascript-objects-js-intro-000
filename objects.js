d9d19fca44f0e9162103292692dd69c4ff2da546
Erick A <servinfma@gmail.com>
2021-01-29 19:53:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artista: "Your song"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  Object.assign(playlist, { artistName: [sonfTitle] })
 }
